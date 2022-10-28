ae44d911cd997d65d3e54fb09f4e28ef02bb06e2
Saineshwar Bageri <saihacksoft@gmail.com>
2022-10-28 20:09:38 +0530

    Commit

--- src/Sharp.CMS/Sharp.CMS.Data/MediaAssets/Command/MediaHistoryCommand.cs
+++ src/Sharp.CMS/Sharp.CMS.Data/MediaAssets/Command/MediaHistoryCommand.cs
@@ -10,10 +10,10 @@ namespace Sharp.CMS.Data.MediaAssets.Command
             _sharpContext = SharpContext;
         }
 
-        public int Add(MenuCategoryModel category)
-        {
-            _sharpContext.MenuCategorys.Add(category);
-            return _sharpContext.SaveChanges();
-        }
+        //public int Add(MenuCategoryModel category)
+        //{
+        //    _sharpContext.MenuCategorys.Add(category);
+        //    return _sharpContext.SaveChanges();
+        //}
     }
 }
